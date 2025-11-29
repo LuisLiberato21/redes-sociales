7e28e883d0576e6a989e80bac19c134dd4f9f4e9
Luis Evelio Garzón Liberato <109185787+LuisLiberato21@users.noreply.github.com>
2025-11-29 03:21:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import joblib
 import numpy as np
+import os
 
 app = FastAPI()
 
 # Cargar modelo
 model = joblib.load("modelo_emociones.pkl")
 
+# Servir frontend
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
 @app.get("/")
 def root():
-    return {"status": "API working successfully 🎉"}
+    return FileResponse(os.path.join("frontend", "index.html"))
 
 @app.post("/predict")
 def predict(data: dict):
-
     # Convertir a array en el mismo orden que tu modelo usa
     valores = np.array([list(data.values())])
-
     pred = model.predict(valores)[0]
-
-    return {
-        "prediction": int(pred)
-    }
+    return {"prediction": int(pred)}
